75fd5ba81ed211419772f173e6dfa5ecd819a98d
Alfredo Medina C <medina8911@gmail.com>
2023-04-18 00:08:07 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ShippingComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    CartComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
